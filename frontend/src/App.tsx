bba351a14f2bad4ca92d1fa66c0d26d0e93c754f
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2024-04-07 13:37:00 +0530

    home page started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
 import UserAuthentication from './Components/UserAuthentication';
+import HomePage from './Components/HomePage';
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' Component={}/> */}
+        <Route path='/' Component={HomePage}/>
         <Route path='/user-authentication' Component={UserAuthentication}/>
-        {/* <Route path='/goodbye' Component={Goodbye}/>
-        <Route path='/signup' Component={Signup}/>
-        <Route path='/login' Component={Login}/> */}
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/Components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as CUI from  "@chakra-ui/react";
+
+const HomePage:React.FC = () => {
+  return (
+  <CUI.Box>
+    {'hello'}
+  </CUI.Box>
+  );
+}
+export default HomePage;
